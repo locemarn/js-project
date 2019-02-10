cfed65ca7d4501a3d0410165934edc7c4614c0ea
locemarn <marcelon27@gmail.com>
2019-02-10 15:50:31 -0200

    componentig and style

---
+++ src/Ripple/Ripple.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import styles from './styles.css'
+
+const Ripple = props => (
+  <>
+    <style>${`${styles}`}</style>
+    <div className="lds-ripple">
+      <div />
+      <div />
+    </div>
+  </>
+)
+
+export default Ripple
